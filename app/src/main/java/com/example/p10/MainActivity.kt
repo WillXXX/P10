4214ef0a98c343fd3390bfb90b770e94fe240f54
Will <will.marcx@gmail.com>
2020-03-02 18:01:52 -0300

    Start

--- app/src/main/java/com/example/p10/MainActivity.kt
+++ app/src/main/java/com/example/p10/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.p10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_main.layoutManager = LinearLayoutManager(this)
+        rv_main.adapter =
     }
 }
--- app/src/main/java/com/example/p10/MainAdapter.kt
+++ app/src/main/java/com/example/p10/MainAdapter.kt
@@ -1,2 +1,23 @@
 package com.example.p10
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : RecyclerView.Adapter<GetViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GetViewHolder {
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onBindViewHolder(holder: GetViewHolder, position: Int) {
+    }
+
+
+}
+
+class GetViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+
+}
