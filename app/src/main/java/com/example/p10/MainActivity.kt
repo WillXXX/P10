053caa2f3c2dd6b3b59d5b2105aecf71a74d6097
Will <will.marcx@gmail.com>
2020-03-16 00:28:11 -0300

    *Project Final

--- app/src/main/java/com/example/p10/MainActivity.kt
+++ app/src/main/java/com/example/p10/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.p10
 import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
