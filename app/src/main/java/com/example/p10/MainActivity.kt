b0c3f62206acce94c2ddbae4bdafe379e6e3e28f
Will <will.marcx@gmail.com>
2020-07-20 22:03:34 -0300

    Revert "*Project Final"
    This reverts commit 053caa2f

--- app/src/main/java/com/example/p10/MainActivity.kt
+++ app/src/main/java/com/example/p10/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.DownloadManager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
--- app/src/main/java/com/example/p10/MainAdapter.kt
+++ app/src/main/java/com/example/p10/MainAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.p_request.view.*
 import kotlinx.android.synthetic.main.screen.view.*
 
 class MainAdapter(val paginaWeb: PaginaWeb) : RecyclerView.Adapter<GetViewHolder>(){
@@ -27,17 +26,12 @@ class MainAdapter(val paginaWeb: PaginaWeb) : RecyclerView.Adapter<GetViewHolder
         holder.view.nomeReposit.text = items.id.toString()
         holder.view.descReposit.text = items.node_id
         holder.view.autor_Name.text = items.name
-//        holder.view.autor_Name2.text = items.name
         holder.view.nome_Completo.text = items.full_name
-//        holder.view.nome_Completo2.text = items.full_name
         holder.view.f_Count.text = items.forks_count.toString()
         holder.view.starCount.text = items.owner.stargazers_count.toString()
 
         val ownerProfileImageView = holder.view.imageView
         Picasso.get().load(items.owner.avatar_url).into(ownerProfileImageView)
-////Informações para tela p_request
-//        val ownerProfileImageView2 = holder.view.imageView2
-//        Picasso.get().load(items.owner.avatar_url).into(ownerProfileImageView2)
     }
 
 }
